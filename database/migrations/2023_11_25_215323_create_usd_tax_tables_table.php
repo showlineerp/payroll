83b72f6940f165c44a46d3724cc18334c709c6de
Eliphas Masuka <masukawebs@gmail.com>
2023-11-26 17:40:08 +0200

    feat:tax table migration

---
+++ database/migrations/2023_11_25_215323_create_usd_tax_tables_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usd_tax_tables', function (Blueprint $table) {
+            $table->id();
+            $table->string('table_type');
+            $table->double('lower_range');
+            $table->double('upper_range');
+            $table->double('multiply_by');
+            $table->double('deduct');
+            $table->string('calculation_info');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usd_tax_tables');
+    }
+};
